827526d35106a4ea3a9e9552b9f010ba525630b1
bantheus <matheuschmidt.b@gmail.com>
2024-02-23 14:25:32 -0300

    🐛 Fixed: mongodb connection

--- src/main/java/com/bantheus/anotaai/config/mongo/MongoDbConfig.java
+++ src/main/java/com/bantheus/anotaai/config/mongo/MongoDbConfig.java
@@ -1,21 +1,29 @@
 package com.bantheus.anotaai.config.mongo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 @Configuration
 public class MongoDbConfig {
 
-  @Bean
-  public MongoDatabaseFactory mongoConfigure(){
-    return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/product-catalog");
-  }
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
 
-  @Bean
-  public MongoTemplate mongoTemplate(){
-    return new MongoTemplate(mongoConfigure());
-  }
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDatabase;
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), mongoDatabase);
+    }
 }
